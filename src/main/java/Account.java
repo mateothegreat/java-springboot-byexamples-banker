b7c8d55ff9b88b26081aacb074fa91b2fe31a4c8
Matthew Davis <matthew@matthewdavis.io>
2019-01-26 08:10:50 -0500

    first commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,16 @@
+public class Account {
+
+    public String accountName;
+    public String accountSurname;
+    public String accountLogin;
+    public String accountPassword;
+    public Double accountFunds;
+
+    @Override
+    public String toString() {
+
+        return "accountName=" + accountName;
+
+    }
+
+}
