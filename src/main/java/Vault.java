2ec956622f0e4e627f3093fcfd8eea1beb7d208e
Matthew Davis <matthew@matthewdavis.io>
2019-01-26 08:59:40 -0500

    documentation

--- src/main/java/Vault.java
+++ src/main/java/Vault.java
@@ -14,16 +14,35 @@ public class Vault {
 
     private static final String VAULT_FILENAME = "vault.csv";
 
+    //
+    // This method retrieves the data out of vault.csv, loops through each item
+    // and creates an Account Object for each.
+    //
+    // It returns an Optional Object which is empty (checked by .isPresent()) if
+    // vault.csv cannot be loaded instead of crashing.
+    //
     public static Optional<List<Account>> getVault() {
 
         List<Account> accounts = new ArrayList<>();
 
         try {
 
+            //
+            // Open up the vault.csv file for reading
+            //
             Reader reader = Files.newBufferedReader(Paths.get(VAULT_FILENAME));
 
+            //
+            // Read in vault.csv and treat it as a CSV object using the commons-csv CSVParser Class
+            //
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());
 
+            //
+            // Loop through each line in the CSV file and create an Account Object
+            // for each.
+            //
+            // We then add it to the List using .add()
+            //
             for (CSVRecord csvRecord : csvParser) {
 
                 Account account = new Account();
@@ -40,22 +59,37 @@ public class Vault {
 
         } catch (IOException e) {
 
+            //
+            // Something bad happened, don't freak out! :)
+            //
             e.printStackTrace();
 
         }
 
+        //
+        // Return either an empty Optional Object or the accounts
+        //
         return accounts.size() > 0 ? Optional.of(accounts) : Optional.empty();
 
     }
 
+    //
+    // Retrieve an account record (from the csv) by it's accountName field value
+    //
     public static Optional<Account> getAccountByName(String name) {
 
         Optional<List<Account>> accounts = getVault();
 
         if (accounts.isPresent()) {
 
+            //
+            // Loop through each account and see if accountName matches
+            //
             for (int i = 0; i < accounts.get().size(); i++) {
 
+                //
+                // We got a match, return the account object now
+                //
                 if (accounts.get().get(i).getAccountName().equals(name)) {
 
                     return Optional.of(accounts.get().get(i));
